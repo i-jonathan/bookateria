1b2d1fbc9d5f95ad322e7af2a61f5dc2319b70f8
Jonathan Farinloye <FarinloyeJonathan@gmail.com>
2018-12-12 14:54:27 +0100

    Login, Signup, Logout complete.

--- account/urls.py
+++ account/urls.py
@@ -4,4 +4,5 @@ from . import views
 urlpatterns = [
     path('login/', views.login, name='login'),
     path('signup/', views.signup, name='signup'),
+    path('logout/', views.logout, name='logout'),
 ]
--- account/views.py
+++ account/views.py
@@ -1,5 +1,47 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.contrib.auth.models import User
+from django.contrib import auth
+from django.contrib.auth.decorators import login_required
 
 # Create your views here.
 def login(request):
-    return render(request, 'account/account.html')
+    if request.method == 'POST':
+        if request.POST['username'] and request.POST['password']:
+            user = auth.authenticate(username=request.POST['username'], password=request.POST['password'])
+            if user is not None:
+                auth.login(request, user)
+                return redirect('home')
+            else:
+                return render(request, 'account/login.html', {'error': 'Username or password is incorrect'})
+        else:
+            return render(request, 'account/login.html', {'error': 'All fields are required'})
+    else:
+        return render(request, 'account/login.html')
+
+# Create your views here.
+def signup(request):
+    if request.method == 'POST':
+        passlen = len(request.POST['passwords'])
+        if request.POST['usernames'] and request.POST['email'] and request.POST['passwords'] and request.POST['passwords1']:
+            if request.POST['passwords'] == request.POST['passwords1']:
+                if passlen > 8:
+                    try:
+                        return render(request, 'account/signup.html', {'error': 'Username has already been taken'})
+                    except User.DoesNotExist:
+                        user = User.objects.create_user(request.POST['usernames'], password=request.POST['passwords'])
+                        auth.login(request, user)
+                        return redirect('home')
+                else:
+                    return render(request, 'account/signup.html', {'error': 'Password must be over 8 characters!'})
+            else:
+                return render(request, 'account/signup.html', {'error': 'Passwords Must Match!'})
+        else:
+            return render(request, 'account/signup.html', {'error': 'All fields are required'})
+    else:
+        return render(request, 'account/signup.html')
+
+
+def logout(request):
+    if request.method == 'POST':
+        auth.logout(request)
+        return redirect('home')
