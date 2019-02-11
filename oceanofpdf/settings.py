057663aee7a02db7d08400f38bfc69ab4b094a09
Jonathan Farinloye <FarinloyeJonathan@gmail.com>
2019-02-11 12:20:55 +0100

    Update settings.py

--- oceanofpdf/settings.py
+++ oceanofpdf/settings.py
@@ -80,7 +80,7 @@ DATABASES = {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': 'urban_prod',
         'USER': 'u_urban',
-        'PASSWORD': 'Test-Passsqwert',
+        'PASSWORD': 'Test-Passqwerty',
         'HOST': 'localhost',
         'PORT': '',
     }
