f36aa8f6f83f9ee1617cdd5f1f9e1db41cbea160
Jonathan Farinloye <FarinloyeJonathan@gmail.com>
2019-02-15 19:30:21 +0100

    What did I do

--- oceanofpdf/settings.py
+++ oceanofpdf/settings.py
@@ -167,7 +167,7 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
 DEFAULT_FILE_STORAGE = 'oceanofpdf.storage_backends.MediaStorage'
 
-EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
+# EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 
 EMAIL_HOST = config('EMAIL_HOST')
 
