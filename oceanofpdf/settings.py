dc8cdf7b7dd865d7b91cdfec51be58b8d327cbd3
Jonathan Farinloye <FarinloyeJonathan@gmail.com>
2019-02-15 23:26:25 +0100

    Almost 24 hours- 4

--- oceanofpdf/settings.py
+++ oceanofpdf/settings.py
@@ -167,15 +167,17 @@ STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'oceanofpdf.storage_backends.MediaStorage'
 
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
-#
+
 EMAIL_HOST = config('EMAIL_HOST')
-#
+
 EMAIL_PORT = config('EMAIL_PORT')
-#
+
 EMAIL_HOST_USER = config('EMAIL_HOST_USER')
-#
+
 EMAIL_HOST_PASSWORD = config('EMAIL_HOST_PASSWORD')
-#
-EMAIL_USE_SSL = True
-#
+
+EMAIL_USE_SSL = False
+
+EMAIL_USE_TLS = False
+
 # DEFAULT_FROM_EMAIL = 'MiniPDF Drive<hello@jayspots.com>'
