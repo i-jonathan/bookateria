ead0752fd14bc907e106b5b733b1a439bd79d76d
Jonathan Farinloye <FarinloyeJonathan@gmail.com>
2019-02-16 20:39:11 +0100

    Search - Adjustments

--- oceanofpdf/settings.py
+++ oceanofpdf/settings.py
@@ -40,7 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.auth',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
-    'django.contrib.messages',Page {{ books.number }} of {{ books.paginator.num_pages }}.
+    'django.contrib.messages',
 
     'django.contrib.staticfiles',
     'whitenoise.runserver_nostatic',
