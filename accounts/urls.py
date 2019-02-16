ac1f078ff46da79daa71401fd5367963985c1bdf
Jonathan Farinloye <FarinloyeJonathan@gmail.com>
2019-02-16 10:06:33 +0100

    Good morning

--- accounts/urls.py
+++ accounts/urls.py
@@ -18,6 +18,7 @@ urlpatterns = [
          ), name='password_reset_confirm'),
     path('reset/done/', auth_views.PasswordResetCompleteView.as_view(
         template_name='accounts/password_reset_complete.html'
-    ), name='password_reset_complete')
+    ), name='password_reset_complete'),
+    path('', auth_views.)
 ]
 
