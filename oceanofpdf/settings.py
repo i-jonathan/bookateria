50e00411ad7113ecbb4735e248ad564a805f8d05
Jonathan Farinloye <FarinloyeJonathan@gmail.com>
2019-02-18 13:17:07 +0100

    FTP partial

--- oceanofpdf/settings.py
+++ oceanofpdf/settings.py
@@ -26,7 +26,8 @@ DEBUG = True
 # TODO change before pushing
 
 ALLOWED_HOSTS = [
-
+    'minipdf.herokuapp.com',
+    '0.0.0.0',
 ]
 
 # Application definition
@@ -91,12 +92,12 @@ DATABASES = {
 db_from_env = dj_database_url.config()
 DATABASES['default'].update(db_from_env)
 # offline work
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
