60f3f2ba08204ed8bc571ce64ac3bdcbec8c6fce
Jay <jay@localhost.localdomain>
2019-02-20 23:10:46 +0100

    heroku heroku

--- oceanofpdf/wsgi.py
+++ oceanofpdf/wsgi.py
@@ -8,9 +8,7 @@ https://docs.djangoproject.com/en/1.11/howto/deployment/wsgi/
 """
 
 import os
-
 from django.core.wsgi import get_wsgi_application
-
+from dj_static import Cling
 os.environ.setdefault("DJANGO_SETTINGS_MODULE", "oceanofpdf.settings")
-
-application = get_wsgi_application()
+application = Cling(get_wsgi_application())
