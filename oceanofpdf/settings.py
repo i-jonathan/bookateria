51162a4d51eb58b096873492ff07f74f47ca8a95
Jonathan Farinloye <FarinloyeJonathan@gmail.com>
2019-03-02 00:18:55 +0100

    Debug oo

--- oceanofpdf/settings.py
+++ oceanofpdf/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = config(bool('DEBUG'))
+DEBUG = bool(config('DEBUG'))
 
 ALLOWED_HOSTS = [
     'bookateria.herokuapp.com',
