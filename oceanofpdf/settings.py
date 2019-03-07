5dfc9ff3ee600abb74203a0feddf92367df6c575
Jonathan Farinloye <FarinloyeJonathan@gmail.com>
2019-03-07 19:43:10 +0100

    remove paramiko and google oauth

--- oceanofpdf/settings.py
+++ oceanofpdf/settings.py
@@ -10,7 +10,6 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 import os
 import dj_database_url
 from decouple import config
-from google.oauth2 import service_account
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
