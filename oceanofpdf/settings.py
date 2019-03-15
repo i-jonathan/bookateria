6f1c7aadb630efa2a1b5a57b62a5c7b4fb3325d0
Jonathan Farinloye <farinloyejonathan@gmail.com>
2019-03-15 23:27:54 +0100

    on_delete patch

--- oceanofpdf/settings.py
+++ oceanofpdf/settings.py
@@ -89,12 +89,12 @@ DATABASES = {
     }
 }
 # offline work
-DATABASES = {
-    'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
-    }
-}
+# DATABASES = {
+#     'default': {
+#         'ENGINE': 'django.db.backends.sqlite3',
+#         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
+#     }
+# }
 
 # Password validation
 # https://docs.djangoproject.com/en/2.1/ref/settings/#auth-password-validators
