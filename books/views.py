238f1a5cc375ff3292156527a3f0bb6d1420d683
Jonathan Farinloye <farinloyejonathan@gmail.com>
2019-03-17 02:08:05 +0100

    fixed duplicate documents

--- books/views.py
+++ books/views.py
@@ -22,30 +22,40 @@ def bookview(request):
 @login_required
 def add(request):
     if request.method == 'POST':
-        book = Books()
-        book.title = request.POST['title']
-        book.author = request.POST['author']
-        book.description = request.POST['description']
-        book.upload_date = timezone.datetime.now()
-        book.uploader = request.user
-        book.pdf = request.FILES['pdf']
-        book.image = request.FILES['image']
-        book.size = book.pdf.size
-        user = User.objects.get(pk=book.uploader_id)
-        user.profile.points += 6
-        user.profile.save()
-        category = request.POST.get('category')
-        typology = Type.objects.get(name__icontains=category)
-        book.typology = typology
-        book.save()
-        for i in request.POST.getlist('faculty'):
-            me = Faculty.objects.get(name__icontains=i)
-            book.faculty.add(me)
+        if Books.objects.filter(title__iexact=request.POST['title']) and \
+                Books.objects.filter(author__iexact=request.POST['author']):
+            diction = {
+                'error': 'Document already Exists',
+                'faculties': Faculty.objects.all(),
+                'levels': Level.objects.all(),
+                'classification': Type.objects.all()
+            }
+            return render(request, 'books/addbook.html', {'dict': diction})
+        else:
+            book = Books()
+            book.title = request.POST['title']
+            book.author = request.POST['author']
+            book.description = request.POST['description']
+            book.upload_date = timezone.datetime.now()
+            book.uploader = request.user
+            book.pdf = request.FILES['pdf']
+            book.image = request.FILES['image']
+            book.size = book.pdf.size
+            user = User.objects.get(pk=book.uploader_id)
+            user.profile.points += 6
+            user.profile.save()
+            category = request.POST.get('category')
+            typology = Type.objects.get(name__icontains=category)
+            book.typology = typology
+            book.save()
+            for i in request.POST.getlist('faculty'):
+                me = Faculty.objects.get(name__icontains=i)
+                book.faculty.add(me)
 
-        for i in request.POST.getlist('level'):
-            lvl = Level.objects.get(name__icontains=i)
-            book.level.add(lvl)
-        return redirect('home')
+            for i in request.POST.getlist('level'):
+                lvl = Level.objects.get(name__icontains=i)
+                book.level.add(lvl)
+            return redirect('home')
     else:
 
         diction = {
@@ -61,9 +71,9 @@ def detail(request, slug):
     return render(request, 'books/detail.html', {'book': book})
 
 
-def download(request, slug):
+def download(request, slug, books_id):
     if request.method == 'POST':
-        book = get_object_or_404(Books, slug=slug)
+        book = get_object_or_404(Books, slug=slug, pk=books_id)
         user = User.objects.get(pk=book.uploader_id)
         user.profile.points += 2
         user.profile.save()
