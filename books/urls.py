2eff3d022b27a01d03443105fb41665d124873ae
Jonathan Farinloye <farinloyejonathan@gmail.com>
2019-03-18 10:12:35 +0100

    fixed search

--- books/urls.py
+++ books/urls.py
@@ -4,8 +4,11 @@ from . import views
 urlpatterns = [
     path('addbook/', views.add, name='addbook'),
     path('all/', views.bookview, name='allbooks'),
-    path('faculties/', views.faculty, name='faculty'),
+    path('search/', views.search, name='search'),
+    # path('faculties/', views.faculty, name='faculty'),
+]
+
+urlpatterns += [
     path('<slug:slug>/', views.detail, name='detail'),
     path('<slug:slug>/download', views.download, name='download'),
-    path('search/', views.search, name='search'),
 ]
