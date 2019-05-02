578910beeca060c62fb8e353f451de6a09b50e3b
Jonathan Farinloye <farinloyejonathan@gmail.com>
2019-05-02 19:36:02 +0100

    :bug: forgot a admin file

--- books/admin.py
+++ books/admin.py
@@ -1,6 +1,5 @@
 from django.contrib import admin
-from .models import Books, Faculty, Type
+from .models import Books, Type
 # Register your models here.
 admin.site.register(Books)
-admin.site.register(Faculty)
 admin.site.register(Type)
