0810fd631f7d621017c1143085127d45644df202
Jay <farinloyejonathan@gmail.com>
2019-09-25 14:26:13 +0100

    :ambulance: Unique user

--- users/models.py
+++ users/models.py
@@ -1,7 +1,9 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
+from django.utils.translation import gettext_lazy as _
 
 
 class User(AbstractUser):
-    pass
-
+    email = models.EmailField(unique=True, error_messages={
+            'unique': _('A user with that email already exists.'),
+        }, db_index=True)
