f88fdf67e90d38e3a4855483cf05762c3bbca274
Jonathan <farinloyejonathan@gmail.com>
2020-06-03 22:14:14 +0100

    Been a while
    Some changes for Django V3. :construction: Work on reset password almost done

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,8 +1,9 @@
-from django.urls import path
+from django.urls import path, include
 from . import views
 
 urlpatterns = [
-    path('login/', views.login, name='login'),
+    path('login/', views.login, name='signin'),
     path('signup/', views.signup, name='signup'),
     path('logout/', views.logout, name='logout'),
+    path('', include('django.contrib.auth.urls'))
 ]
